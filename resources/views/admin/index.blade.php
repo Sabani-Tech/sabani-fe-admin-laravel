2606951e6f09c2c28b4eed234557bcc005ee000c
syamsulapp <samsul.marif100@gmail.com>
2024-09-14 10:04:42 +0800

    init template

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,7 @@
+@extends('admin.layouts.app')
+
+@section('title', 'Welcome Admin Dashboard')
+
+@section('content')
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('admin')->group(function () {
+    Route::get('dashboard', function () {
+        return view('admin.index');
+    })->name('admin.dashboard');
+});
